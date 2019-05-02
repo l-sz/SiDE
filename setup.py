873674ea904e656a91b5a718e1872c3aca04f587
László Szűcs <laszlo.szucs@mpe.mpg.de>
2019-05-02 17:02:49 +0200

    chg: move from distutil to setuptools

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
+
+try:
+   import galario
+   print ('galario found in path')
+except ImportError:
+   print ('WARN: galario must be installed manually: https://github.com/mtazzari/galario')
 
 setup(name='SimpleDiskEnvFit',
       version='0.1',
-      requires=['radmc3dPy', 'corner', 'galario', 'emcee', 'uvplot'],
+      install_requires=['radmc3dPy', 'corner', 'emcee', 'uvplot'],
+      extras_require={'galario':['galario']},
+      provides=['SimpleDiskEnvFit'],
       description='RADMC3D based Class 0/I/II protostar fitting tool.',
       author='Laszlo Szucs',
       author_email='laszlo.szucs@mpe.mpg.de',
+      license='GPLv2',
       url='https://gitlab.mpcdf.mpg.de/szucs/SimpleDiskEnvFit',
       packages=['SimpleDiskEnvFit'],
       package_data={'SimpleDiskEnvFit':['lnk_files/*.lnk']},
